91f0736cc5e2d3ddea2b4724fb1a97756f6296f2
Aleksandar Simovic <alexander.simovic@gmail.com>
2019-02-28 15:58:13 +0100

    adding minor descriptions and more proper code

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# Example Application SAM template with a Stripe component
+# Sample App SAM template with a Stripe component
 
-## Building serverless apps with components from the AWS Serverless Application Repository
+### Building serverless apps with components from the AWS Serverless Application Repository
 
 Includes:
 
-- `ShowProduct`, a serverless Function
+- `ShowProduct`, a AWS Lambda Function to display a single product
+- `ProductsTable`, a AWS DynamoDB Table to store products
+- `apilambdastripecharge`, a AWS Serverless Application Repository Component, that exposes a callback to get invoked by Stripe Checkout when your customer clicks Buy on your product.
--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-
+const AWS = require('aws-sdk'),
+    dynamoDb = new AWS.DynamoDB.DocumentClient(),
+    processResponse = require('./process-response'),
+    TABLE_NAME = 'products';
 
 exports.handler = event => {
-  
+  if (event.httpMethod === 'OPTIONS') {
+		return Promise.resolve(processResponse(true));
+    }
+    const requestedProductId = event.pathParameters.productId;
+    if (!requestedProductId) {
+        return Promise.resolve(processResponse(true, `Error: You missing the productId parameter`, 400));
+    }
+
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+          productId: requestedProductId
+        }
+    }
+    return dynamoDb.get(params)
+    .promise()
+    .then(response => processResponse(true, response.Item))
+    .catch(err => {
+        console.log(err);
+        return processResponse(true, 'dynamo-error', 500);
+    });
 };
